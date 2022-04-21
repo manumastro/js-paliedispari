e92862f07a28293ec59c9fa06ac4160b3f90476f
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-04-21 11:24:02 +0200

    correzione esercizio pari o dispari

--- script.js
+++ script.js
@@ -23,8 +23,76 @@ function initVerifica(){
 function giraParola(str){
   let reverseString = '';
   
+  console.log(str.split('').reverse().join(''));
+
   for(let i = str.length - 1; i >= 0; i--){
     reverseString += str[i];
   }
   return reverseString;
 }
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+/*
+PARI DISPARI
+
+1. Scegliere pari o dispari dalla select 
+2. Giocare un numero da 1 a 5
+3. Fare estrarre al pc un numero random da 1 a 5
+4. Verificare il vincitore
+
+*/
+
+document.getElementById('gioca').addEventListener('click', initPariDispari);
+
+function initPariDispari(){
+  const userChoice = document.getElementById('choice').value;
+  const userNumber = parseInt(document.getElementById('numero').value);
+  const pcNumber = getRandomNumber(1, 5);
+  const sum = userNumber + pcNumber;
+  let outputMsg;
+  if(userChoice === pariDispari(sum)){
+    outputMsg = `
+    <h3>Hai vinto</h3>
+    <p>Hai giocato <strong>${userNumber}</strong> e scelto <strong>${userChoice}</strong> </p>
+    <p> il PC ha giocato: <strong>${pcNumber}</strong></p>
+    `;
+  }else{
+    outputMsg = `
+    <h3>Hai vinto</h3>
+    <p>Hai giocato <strong>${userNumber}</strong> e scelto <strong>${userChoice}</strong> </p>
+    <p> il PC ha giocato: <strong>${pcNumber}</strong></p>
+    `;
+  }
+  document.getElementById("output-paridispari").innerHTML = outputMsg;
+}
+/**
+ * Generatore di numeri random
+ * @param {number} min 
+ * @param {number} max 
+ * @returns 
+ */
+function getRandomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+ * filtro pari/dispari
+ * @param {number} n 
+ * @returns 
+ */
+function pariDispari(n){
+  if(n % 2) return 'dispari';
+  return 'pari';
+}
+
+
+
+
