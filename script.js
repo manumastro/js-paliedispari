2386df2e51fa35d18543b25b9490e10e2b27a2bc
manumastro <emanuele.mastronardi.2002.12@gmail.com>
2022-04-21 10:11:04 +0200

    correzione esercizio della parte palindromo

--- script.js
+++ script.js
@@ -1,63 +1,30 @@
-/* Palidroma
-Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma
-Bonus: L’inserimento avviene tramite un campo input */
+/*
+1. evento scatenato dal click del bottone
+2. leggere la parola inserita nel campo input
+3. girarla e verificare se è palindroma
+4. stampare l'output
+*/
 
+document.getElementById('verifica').addEventListener('click', initVerifica);
 
-const output = document.getElementById("palindromo");
-
-
-let word = prompt("Inserire una parola");
-
-let wordReverse = reverseFunction(word);
-
-if(word == wordReverse){
-  output.innerHTML = "la parola è palindroma";
-}else{
-  output.innerHTML = "la parola non è palindroma";
-}
-
-function reverseFunction(stringa){
-  let stringaInversa = stringa.split('').reverse().join('');
-  return stringaInversa;
-} 
-
-
-/* Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto.
-Bonus: L’inserimento avviene tramite un campo input
- */
-
-const pariDispariUser = prompt("Scegli pari o dispari");
-const numUser = parseInt(prompt("Inserire un numero da 1 a 5"));
-
-let numPC = numPcRandom();
-
-
-let pariDispariSomma = pariDispariSommaFunction();
-
-
-if(pariDispariUser === pariDispariSomma){
-  console.log("Hai vinto");
-}
-else{
-  console.log("Hai perso");
-}
-
-function pariDispariSommaFunction(){
-  let sum = numUser + numPC;
-  if(sum % 2 === 0){
-    return 'pari';
+function initVerifica(){
+  const parola = document.getElementById('parola').value;
+  const parolaGirata = giraParola(parola);
+  
+  let outputMsg;
+  if(parola.toLowerCase() === parolaGirata.toLowerCase()){
+    outputMsg = `La parola "${parola}" è palindroma`;
   }else{
-    return 'dispari';
+    outputMsg = `La parola "${parola}" non è palindroma`;
   }
+  document.getElementById("output-palindroma").innerHTML = outputMsg;
 }
 
-function numPcRandom(){
-  let random = Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
-  return random;
+function giraParola(str){
+  let reverseString = '';
+  
+  for(let i = str.length - 1; i >= 0; i--){
+    reverseString += str[i];
+  }
+  return reverseString;
 }
